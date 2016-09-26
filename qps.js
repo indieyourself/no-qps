145844b633eccd1b02da6a6a241339fe4f96cef7
HanWenfang <eehanwenfang@gmail.com>
2016-09-26 11:46:59 +0800

    qps

---
+++ qps.js
@@ -0,0 +1,27 @@
+function QPS( requestNum ) {
+	this.requestNum = requestNum;
+	this.counter = 0;
+	this.startTime = 0;
+	this.endTime = 0;
+	this.duration = 0;
+}
+
+QPS.prototype.start = function() {
+	if (this.startTime === 0) {
+		this.startTime = Date.now();
+	}
+}
+
+QPS.prototype.count = function() {
+	++this.counter;
+
+	if (this.counter == this.requestNum) {
+		this.endTime = Date.now();
+		this.duration = this.endTime - this.startTime;
+		console.log(
+			`requestNum: ${this.requestNum}\n
+			   startTime: ${this.startTime} endTime: ${this.endTime}\n
+			   QPS: ${requestNum*1000/duration}\n
+			`);
+	};
+}
